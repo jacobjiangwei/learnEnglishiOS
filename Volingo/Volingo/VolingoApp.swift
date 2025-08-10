690b66900ab715cc25bd2f928c3048d82a8cbe2f
Jacob Jiang <jacob.jiang@microsoft.com>
2025-08-10 16:43:19 +0800

    initial

---
+++ Volingo/Volingo/VolingoApp.swift
@@ -0,0 +1,32 @@
+//
+//  VolingoApp.swift
+//  Volingo
+//
+//  Created by jacob on 2025/8/10.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VolingoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Volingo/VolingoTests/VolingoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VolingoTests.swift
+//  VolingoTests
+//
+//  Created by jacob on 2025/8/10.
+//
+
+import Testing
+@testable import Volingo
+
+struct VolingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
